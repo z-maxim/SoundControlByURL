2ad46f305654dfc41328e61203bfee5e460d3dbe
Shshknv <shshknv@gmail.com>
2018-06-30 00:36:16 +0300

    Добавлено логирование запуска треков

--- Controllers/SoundController.cs
+++ Controllers/SoundController.cs
@@ -7,20 +7,27 @@ using System.Net.Http;
 using System.Threading;
 using System.Web.Http;
 using System.IO;
+using System.Configuration;
+using System.Collections.Specialized;
+using WebApplication.Models;
 
-namespace SoundService
+
+namespace WebApplication.Controllers
 {
     public class SoundController : ApiController
     {
 
-        private static WaveOut waveOut;
-        private List<string> list;
+        private static WaveOutEvent waveOut;
+        public List<string> list;
+        public List<Track> tracks = new List<Track>();
         private static Random rnd = new Random();
+        private string path = ConfigurationManager.AppSettings.Get("Path");
+
 
         [HttpGet]
-        public void Play()
+        public void Play(int id, int trackid)
         {
-            var t = new Thread(() => StartPlay());
+            var t = new Thread(() => StartPlay(id, trackid));
             t.Start();
         }
 
@@ -28,36 +35,55 @@ namespace SoundService
         public void Stop()
         {
             if (waveOut != null && waveOut.PlaybackState == PlaybackState.Playing)
-            {
                 waveOut.Stop();
-                waveOut.Dispose();
-                waveOut = null;
-            }
         }
 
-        private void StartPlay()
+        public void Catalog()
         {
-            if (waveOut == null)
+            String[] arr;
+            list = new List<String>();
+            arr = Directory.GetFiles("C:\\Media", "*.mp3");
+            for (int i = 0; i < arr.Length; i++)
             {
-                list = new List<String>();
-                String[] arr = Directory.GetFiles("C:\\Media", "*.mp3");
-                for (int i = 0; i < arr.Length; i++)
-                {
-                    list.Add(arr[i]);
-                }
-                waveOut = new WaveOut();
-                int r = rnd.Next(list.Count);
-                var mp3Reader = new Mp3FileReader(list[r]);
-                waveOut.Init(mp3Reader);
+                list.Add(arr[i]);
+                tracks.Add(new Track() { Id = i + 1, Name = arr[i] });
             }
-            waveOut.Play();
         }
 
-        //private void Init()
-        //{
-        //    list = new List<String>();
-        //    String[] arr = Directory.GetFiles("C:\\Media", "*.mp3");
-        //}
+        public void Log(string message)
+        {
+            System.IO.File.AppendAllText("D:\\log/" + 
+			DateTime.Now.ToString("yyyyMMdd") + ".log", DateTime.Now + " " + message + "\r\n");
+
+        }
+        [HttpGet]
+        public IEnumerable<Track> GetAllTracks()
+        {
+            Catalog();
+            return tracks;
+        }
+
+
+        [HttpGet]
+        private void StartPlay(int location, int trackid)
+        {
+
+            if (waveOut != null)//waveOut.PlaybackState == PlaybackState.Playing
+            {
+                waveOut.Dispose();
+            }
+            Catalog();
+            waveOut = new WaveOutEvent();
+            waveOut.DeviceNumber = location;
+            //int r = rnd.Next(list.Count);
+            var mp3Reader = new Mp3FileReader(list[trackid - 1]);
+            waveOut.Init(mp3Reader);
+            // }
+            waveOut.Play();
+            string message = "Track №" + trackid + " started on device №" + location;
+            Log(message);
+        }
+
     }
 }
 
