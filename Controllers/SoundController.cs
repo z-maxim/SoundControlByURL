9472a65bec7ff4b420672a4d55f2f952051d9d1c
Won-der-dan <mylodlyadobrochana@gmail.com>
2018-07-20 11:14:07 +0300

    Убран 'хардкод', потоки и WaveOutEvent-ы хранятся в массивах

--- Controllers/SoundController.cs
+++ Controllers/SoundController.cs
@@ -16,45 +16,41 @@ namespace WebApplication.Controllers
 {
     public class SoundController : ApiController
     {
-        public static WaveOutEvent waveOut1, waveOut2;
+        public static WaveOutEvent[] waveOut;
+        public Thread[] t;
         public List<string> list;
         public List<Track> tracks = new List<Track>();
         private static Random rnd = new Random();
         private string path; //Директория со звуковыми файлами
 
 
+        //Запускает отдельный поток для каждого WaveOutEvent
         [HttpGet]
         public void Play(int id, int trackid)
         {
-            //waveOut1 = new WaveOutEvent();
-            //waveOut2 = new WaveOutEvent();
-            var t1 = new Thread(() => StartPlay1(0, trackid));
-            var t2 = new Thread(() => StartPlay2(1, trackid));
-            t1.Start();
-            t2.Start();
+            if (waveOut == null)
+            {
+                waveOut = new WaveOutEvent[2];
+            }
+            if (t == null)
+            { 
+                t = new Thread[2];
+                for (int i = 0; i < waveOut.Length; i++)
+                {
+                    t[i] = new Thread(() => StartPlay1(id, trackid));
+                }
+            }
+            t[id].Start();
         }
 
-        //[HttpGet]
-        //public void Stop(int id)
-        //{
-        //    if (waveOut[id] != null && waveOut[id].PlaybackState == PlaybackState.Playing)
-        //    {
-        //        waveOut[id].Stop();
-        //    }
-
-
-        //}
 
         [HttpGet]
-        public void Stop()
+        public void Stop(int id)
         {
-            //if (waveOut1 != null && waveOut1.PlaybackState == PlaybackState.Playing)
-            //{
-                waveOut1.Stop();
-                waveOut2.Stop();
-            //}
-
-
+            if (waveOut[id] != null && waveOut[id].PlaybackState == PlaybackState.Playing)
+            {
+                waveOut[id].Stop();
+            }
         }
 
         public void Catalog()
@@ -98,37 +94,19 @@ namespace WebApplication.Controllers
         [HttpGet]
         private void StartPlay1(int location, int trackid)
         {
-            //if (waveOut1 != null)
-            //{
-            //    waveOut1.Dispose();
-            //}
+            if (waveOut[location] != null)
+            {
+                waveOut[location].Stop();
+            }
             Catalog();
-            waveOut1 = new WaveOutEvent();
-            waveOut1.DeviceNumber = location;
+            waveOut[location] = new WaveOutEvent();
+            waveOut[location].DeviceNumber = location;
             //int r = rnd.Next(list.Count);
             var mp3Reader1 = new Mp3FileReader(list[trackid-1]);
-            waveOut1.Init(mp3Reader1);
+            waveOut[location].Init(mp3Reader1);
             // }
-            waveOut1.Play();
+            waveOut[location].Play();
         }
-
-        [HttpGet]
-        private void StartPlay2(int location, int trackid)
-        {
-            //if (waveOut2 != null)
-            //{
-            //    waveOut2.Dispose();
-            //}
-            Catalog();
-            waveOut2 = new WaveOutEvent();
-            waveOut2.DeviceNumber = location;
-            //int r = rnd.Next(list.Count);
-            var mp3Reader2 = new Mp3FileReader(list[trackid - 1]);
-            waveOut2.Init(mp3Reader2);
-            // }
-            waveOut2.Play();
-        }
-
     }
 }
 
