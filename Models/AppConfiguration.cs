e9f5250d0058ed07cfc31710a316983ba851e682
Won-der-dan <mylodlyadobrochana@gmail.com>
2018-11-18 13:46:12 +0300

    Comments added

--- Models/AppConfiguration.cs
+++ Models/AppConfiguration.cs
@@ -4,10 +4,15 @@ namespace WebApplication.Models
 {
     public class AppConfiguration
     {
+        //path to Sound Files
         public string MediaPath { get; set; }
+        //path to logs catalogue
         public string LogPath { get; set; }
+        //path to Alert files
         public string AlertPath { get; set; }
+        //Names of audio outputs assigned to rooms
         public List<string> RoomNames { get; set; }
+        //List of alerts
         public List<Alert> Alerts { get; set; }
     }
 
